be9d045962c9a472ae0dccdcad98c0f1a5e5a0dc
JanurSyahPutra <janursyahputra@gmail.com>
2022-09-18 10:24:20 +0700

    promise.js modified

--- promise.js
+++ promise.js
@@ -1,8 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+  let IXX = await promiseTheaterIXX();
+  let VGC = await promiseTheaterVGC();
+
+  const hitungemosi = 
+    IXX.filter((emosiIXX) => emosiIXX.hasil === emosi).length + VGC.filter((emosiVGC) => emosiVGC.hasil === emosi).length;
+    return hitungemosi; 
+}
 
 module.exports = {
   promiseOutput,
 };
+
